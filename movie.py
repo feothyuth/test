4831f6199c415f579935724385afdb4fcb482360
feothyuth <129170330+feothyuth@users.noreply.github.com>
2023-07-26 14:41:10 +0700

    Update movie.py

--- movie.py
+++ movie.py
@@ -4,19 +4,33 @@ from sklearn.metrics.pairwise import cosine_similarity
 # Load the movie ratings dataset
 ratings_data = pd.read_csv('ratings.csv')
 
-# Create a user-movie matrix
-user_movie_matrix = ratings_data.pivot(index='user_id', columns='movie_id', values='rating').fillna(0)
+# Load the movie metadata containing movie_id, movie_title, and genres
+metadata = pd.read_csv('movies_metadata.csv', usecols=['id', 'title', 'genres'])
 
-# Calculate the similarity matrix using cosine similarity
-similarity_matrix = pd.DataFrame(cosine_similarity(user_movie_matrix))
+# Rename the 'id' column to 'movie_id' to match the ratings_data column
+metadata.rename(columns={'id': 'movie_id'}, inplace=True)
 
-# Function to get top N movie recommendations for a given user
+# Merge the ratings_data with the metadata based on 'movie_id'
+ratings_data = pd.merge(ratings_data, metadata, on='movie_id')
+
+# Create a user-movie-genre matrix
+user_movie_matrix = ratings_data.pivot_table(index='user_id', columns=['movie_id', 'title'], values='rating', fill_value=0)
+
+# Remove the 'title' index level to get a multi-index dataframe with 'user_id' and 'movie_id' as indices
+user_movie_matrix = user_movie_matrix.droplevel('title', axis=1)
+
+# Calculate the similarity matrix using cosine similarity and genre preferences
 def get_movie_recommendations(user_id, top_n=5):
-    user_ratings = user_movie_matrix.loc[user_id].values.reshape(1, -1)
-    similarity_scores = cosine_similarity(user_ratings, user_movie_matrix)[0]
+    user_ratings = user_movie_matrix.loc[user_id]
+    user_genres = user_ratings.dot(ratings_data['genres']).sum()  # Calculate user's genre preferences
+
+    # Calculate the similarity matrix using both ratings and genres
+    similarity_scores = cosine_similarity(user_ratings, user_movie_matrix) + user_genres
+
+    # Find the top N similar movies
     top_movie_indices = similarity_scores.argsort()[::-1][:top_n]
     top_movie_ids = user_movie_matrix.columns[top_movie_indices]
-    movie_recommendations = ratings_data[ratings_data['movie_id'].isin(top_movie_ids)]['movie_title'].unique()
+    movie_recommendations = ratings_data[ratings_data['movie_id'].isin(top_movie_ids)]['title'].unique()
     return movie_recommendations
 
 # Example usage
