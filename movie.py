c8c6823e79b9b4bef0e10704d23201d97119add3
feothyuth <129170330+feothyuth@users.noreply.github.com>
2023-07-30 10:56:52 +0700

    Update movie.py

--- movie.py
+++ movie.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 import imdb
+# not finished
 
 # Load the movie ratings dataset
 ratings_data = pd.read_csv('ratings.csv')
